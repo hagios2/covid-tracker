a4780698e0a47292028df68df12f54e6fa7f2060
Emmanuel Oteng Wilson <hagioswilson@gmail.com>
2021-12-17 13:16:44 +0000

    finished stting up covid tracker

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="text-center bg-blue-800 p-4 text-white mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3">
+      <i class="fa fa-viruses"></i> Covid-19 Tracker
+    </div>
+  </header>
+
+</template>
+
+<script>
+export default {
+  name: {
+    name: 'Header'
+  },
+}
+</script>
